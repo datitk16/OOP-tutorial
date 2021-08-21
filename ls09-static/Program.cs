611541c283e56281722a79908639ca2330ef1cc5
Dat <datndd2604@gmail.com>
2021-08-21 20:40:50 +0700

    static

--- ls09-static/Program.cs
+++ ls09-static/Program.cs
@@ -7,17 +7,17 @@ namespace ls09_static
     {
         static void Main(string[] args)
         {
-            // BienTinh.CountNumber c1 = new BienTinh.CountNumber();
-            //  BienTinh.CountNumber c2 = new BienTinh.CountNumber();
-            // c1.count();// CountNumber.num = 1;
-            // c2.count();// CountNumber.num = 2;
-            // c1.count();// CountNumber.num = 3;
-            // Console.WriteLine(c2.getNum());
+            BienTinh.CountNumber c1 = new BienTinh.CountNumber();
+            BienTinh.CountNumber c2 = new BienTinh.CountNumber();
+            c1.count();// CountNumber.num = 1;
+            c2.count();// CountNumber.num = 2;
+            c1.count();// CountNumber.num = 3;
+            Console.WriteLine(c2.getNum());
 
 
-         int ketqua =   PhuongThucTinh.MethodStatic.Sum(1,2); // trả về 3 - gọi phương thức mà không cần tạo đối tượng
+            int ketqua = PhuongThucTinh.MethodStatic.Sum(1, 2); // trả về 3 - gọi phương thức mà không cần tạo đối tượng
 
-         Console.WriteLine(ketqua);
+            Console.WriteLine(ketqua);
         }
     }
 }
